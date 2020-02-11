9dbe35b1f9a4d9d798a23fb730c108b924df54f9
Wenceslao Llobet <wenceslao.llobet@adevinta.com>
2020-02-11 16:58:32 +0100

    Red: 				( lines added = 4 )
    - we expect the transaction to fail one time and then succeed

--- tm/src/test/java/io/seata/tm/api/DefaultGlobalTransactionShould.java
+++ tm/src/test/java/io/seata/tm/api/DefaultGlobalTransactionShould.java
@@ -1,11 +1,15 @@
 package io.seata.tm.api;
 
 import io.seata.core.exception.TransactionException;
+import io.seata.core.model.GlobalStatus;
 import io.seata.core.model.TransactionManager;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class DefaultGlobalTransactionShould {
 
@@ -14,6 +18,12 @@ class DefaultGlobalTransactionShould {
   @Test
   void should_repeat_attempts() throws TransactionException {
     DefaultGlobalTransaction defaultGlobalTransaction = new DefaultGlobalTransaction("",null,null, transactionManager);
+    when(transactionManager.commit(""))
+        .thenThrow(new TransactionException(""))
+        .thenReturn(GlobalStatus.Committed);
+
     defaultGlobalTransaction.commit();
+
+    verify(transactionManager,times(2)).commit("");
   }
 }
